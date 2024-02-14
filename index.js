8947ad1788b226e07dd48f462ac5659686b6e8c4
Pedro Mendonça Dias <125631065+p-mendoncad@users.noreply.github.com>
2024-02-14 08:00:39 +0000

    Apresentação CSS e HTML corrigidos

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-let slideIndexApresenta = 0;
-autoShowSlidesApresenta();
-
-function autoShowSlidesApresenta() {
-  let i;
-  let slidesApresenta = document.getElementsByClassName("apresentaSlide");
-  for (i = 0; i < slidesApresenta.length; i++) {
-    slidesApresenta[i].style.display = "none";
-  }
-  slideIndexApresenta++;
-  if (slideIndexApresenta > slidesApresenta.length) {slideIndexApresenta = 1}
-  slidesApresenta[slideIndexApresenta-1].style.display = "block";
-  setTimeout(autoShowSlidesApresenta, 2000);
-}
-
 let slideIndexCliente = 0;
 autoShowSlidesCliente();
 
